970051a5eaee14e2dd36465e9747645b721a8cd4
Alejandra Cáceres <alejandraicaceres@gmail.com>
2022-12-01 10:25:39 -0300

    Add Pokemon entity - Configure MongooseModule

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,19 @@
-export class Pokemon {}
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  nmbr: number;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
